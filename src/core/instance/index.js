dada9bfe4b1da7b4db1ca1dc0828709cd23db3d4
Lgee <1565692947@qq.com>
2022-10-12 18:00:18 +0800

    feat(parse): a
    a
    
    BREAKING CHANGE: asd
    
    asd

--- src/core/instance/index.js
+++ src/core/instance/index.js
@@ -1,23 +1,24 @@
-import { initMixin } from './init'
-import { stateMixin } from './state'
-import { renderMixin } from './render'
-import { eventsMixin } from './events'
-import { lifecycleMixin } from './lifecycle'
-import { warn } from '../util/index'
+import { initMixin } from "./init";
+import { stateMixin } from "./state";
+import { renderMixin } from "./render";
+import { eventsMixin } from "./events";
+import { lifecycleMixin } from "./lifecycle";
+import { warn } from "../util/index";
 
-function Vue (options) {
-  if (process.env.NODE_ENV !== 'production' &&
-    !(this instanceof Vue)
-  ) {
-    warn('Vue is a constructor and should be called with the `new` keyword')
+function Vue(options) {
+  //判断是否以new关键字创建的vue实例
+  console.log(this);
+  if (process.env.NODE_ENV !== "production" && !(this instanceof Vue)) {
+    warn("Vue is a constructor and should be called with the `new` keyword");
   }
-  this._init(options)
+  // 调用initMixin为Vue实例原型挂载的_init
+  this._init(options);
 }
 
-initMixin(Vue)
-stateMixin(Vue)
-eventsMixin(Vue)
-lifecycleMixin(Vue)
-renderMixin(Vue)
+initMixin(Vue);
+stateMixin(Vue);
+eventsMixin(Vue);
+lifecycleMixin(Vue);
+renderMixin(Vue);
 
-export default Vue
+export default Vue;
